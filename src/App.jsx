acb86cb53520b00b664268cf96c1e7c948a1c3af
Wasabis <hello@psychas.xyz>
2024-06-14 16:36:40 +0200

    data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
 
-  const bookApi = axios.get('https://api.nytimes.com/svc/books/v3/lists/full-overview.json?api-key=')
+  const fetchData = async () => {
+    const response = await axios.get(
+      'https://api.nytimes.com/svc/books/v3/lists/full-overview.json?api-key='
+    );
 
+    setData(response);
+    console.log(response.data.results);
+  };
 
-  return (
-    <>
+  useEffect(() => {
+    // fetch data
+    fetchData();
+  }, []);
 
-
-
-    </>
-  )
+  return <>Hello there</>;
 }
 
-export default App
+export default App;
+
+// class App extends React.Component {
+//   constructor(props) {
+//     this.bookData =[];
+//   }
+
+//   componentDidMount(){
+//     // fecth data
+//     // setState()
+//   }
+//   component­Will­Unmount(){}
+//   componentDidUpdate()
+
+//    return <></>;
+
+// }
+
+// useEffect(() => {
+//   // mounting  + updating
+//   // componentDidMount()
+//   // fetch data
+//   fetchData();
+
+//   return () => {
+//     // unmounting component
+//     // componentWillUnmount()
+//     // cleanup
+//   };
+// }, []);
